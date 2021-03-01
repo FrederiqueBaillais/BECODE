9dc896ad8d31c3324b523cf4aea76c538f95dd66
Frederique Baillais <frederique.baillais@gmail.com>
2021-03-01 09:47:03 +0100

    commit de fin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,24 @@
-const quote = document.getElementById('quote');
-const load = document.getElementById('load');
-const image = document.getElementById('image');
-const author = document.getElementById('author');
+let quote = document.getElementById('quote');
+let load = document.getElementById('load');
+let image = document.getElementById('image');
+let author = document.getElementById('author');
 
-function recuperation() {
-    // https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest
-    const requete = new XMLHttpRequest();
-    // ' load ' se déclenche lorsqu'une transaction XMLHttpRequest se termine correctement
-    requete.load = affichage;
-    // ' open ' initialise une requête
-    // https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest/open
-    // XMLHttpRequest.open(method, url, async)
-    requete.open('get', 'https://thatsthespir.it/api', true)
-        .then(res => res.json())
-        .then((res) => { selectNote(quote) })
-        .catch(() => console.log('Requête échouée'));
-    // ' send ' envoie la requête
-    // XMLHttpRequest.send(body)
-    requete.send();
+const recuperation = () => {
+    const req = new XMLHttpRequest(); // https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest
+    console.log('test');
+    req.onload = reqListener; // ' onload ' se déclenche lorsqu'une transaction XMLHttpRequest se termine correctement
+    // ' open ' initialise une requête // XMLHttpRequest.open(method, url, async)
+    req.open('get', 'https://thatsthespir.it/api', true); // https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest/open
+    req.send(); // ' send ' envoie la requête // XMLHttpRequest.send(body)
 };
-const affichage = {
+const reqListener = response => {
+    console.log('Hello');
     let temp = JSON.parse(response.currentTarget.response);
     quote.innerHTML = temp.quote;
     author.textContent = temp.author;
     image.src = temp.photo;
 };
 window.onload = () => {
-    loadButton.onclick = recuperation();
+    load.onclick = recuperation();
 };
 recuperation();
-
-
-
-
-
-
-/*load.addEventListener('click', quotation());*/
